c8043b2d05cfa8b78082ae5ebc3d6bbfdae041d5
Neha Kennard <kennard@cs.umass.edu>
2021-07-17 22:35:58 -0400

    pages update

---
+++ process_songs.sh
@@ -0,0 +1,15 @@
+mkdir -p assets/qr-codes
+mkdir -p _formatted_songs
+
+git rm assets/qr-codes/*
+git rm _formatted_songs/*
+
+python reformat_song_pages.py
+
+git add assets/qr-codes/*
+git add _formatted_songs/*
+
+python reformat_song_pages.py
+
+git commit -am 'pages update'
+git push
