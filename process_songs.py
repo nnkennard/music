2a6aa24e030704800f76f9179e7d95c0d99cdacc
Neha Kennard <kennard@cs.umass.edu>
2021-07-17 22:31:50 -0400

    everythign done except contents

--- process_songs.py
+++ process_songs.py
@@ -3,6 +3,12 @@ import frontmatter
 
 import qrcode
 
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
 def fix_word(word):
   return "".join([i.lower() for i in word if i.isalpha()])
 
@@ -20,22 +26,60 @@ def get_permalink(title, existing_songs):
 def format_content(text):
   return "<br>\n".join(text.split("\n"))
 
+def make_and_save_qrcode(link_text, file_name):
+  img = qrcode.make(link_text)
+  img.save("assets/qr-codes/{0}.png".format(file_name))
+
+def write_formatted_file(permalink, post):
+  with open("".join(["_formatted_songs/", permalink, ".md"]), 'w') as f:
+    f.write(frontmatter.dumps(post))
+
+class Field(object):
+  TITLE = "title"
+  ARTIST = "artist"
+
+  PERMALINK = "permalink"
+  LAYOUT = "layout"
+
+  USER_DEFINED = [TITLE, ARTIST]
+
+defaults = {
+  Field.TITLE: "Untitled",
+  Field.ARTIST: "Unknown artist"
+}
+
+def error(field, filename):
+  print("The file {0} needs a {1} in the frontmatter".format(filename,
+  field))
+
+def fix_and_write(post, permalink):
+  post[Field.LAYOUT] = "song"
+  post[Field.PERMALINK] = permalink
+  for field in Field.USER_DEFINED:
+    if field not in post:
+      error(field, song_file)
+      post[field] = defaults[field]
+  post.content = format_content(post.content)
+  write_formatted_file(permalink, post)
+
+
 def main():
 
-  formatted_songs = {}
+  stream = open("_config.yml", 'r')
+  site_config = yaml.load(stream, Loader)
+  base_url = site_config["url"]
+
+  # QR code for main page
+  make_and_save_qrcode(base_url, "Home")
+
+  permalink_list = []
 
   for song_file in glob.glob("_songs/*"):
     post = frontmatter.load(song_file)
-    permalink = get_permalink(post['title'], formatted_songs)
-    post['permalink'] = permalink
-    img = qrcode.make("localhost:4000/"+permalink)
-    img.save("qr-codes/{0}.png".format(permalink))
-
-    post['layout'] = 'song'
-    post.content = format_content(post.content)
-    formatted_songs[permalink] = post
-    with open("".join(["_formatted_songs/", permalink, ".md"]), 'w') as f:
-      f.write(frontmatter.dumps(post))
+    permalink = get_permalink(post['title'], permalink_list)
+    make_and_save_qrcode(base_url+permalink, permalink)
+    fix_and_write(post, permalink)
+    permalink_list.append(permalink)
 
 
 if __name__ == "__main__":
