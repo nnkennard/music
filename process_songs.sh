a7b0209d9f9be7c02ce12c8c4001996d1f095c4f
Neha Kennard <nnayak@umass.edu>
2022-05-22 21:41:56 -0400

    some cleanup

--- process_songs.sh
+++ process_songs.sh
@@ -3,13 +3,14 @@ source ve/bin/activate
 git rm assets/qr-codes/*
 git rm _formatted_songs/*
 
+mkdir -p assets/qr-codes/
+mkdir -p _formatted_songs/
+
 python reformat_song_pages.py
 
 git add assets/qr-codes/*
 git add _formatted_songs/*
 git add _songs/*
-
-python reformat_song_pages.py
 
 git commit -am 'pages update'
 git push
